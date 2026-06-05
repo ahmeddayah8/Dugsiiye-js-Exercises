6bf7b07dda3728a6aca5d76ebe36148ff2f7f18a
ahmeddayah8 <ahmeddayah449@gmail.com>
2026-06-05 23:31:18 +0100

    Exercise 10

---
+++ Exercise 10/main.js
@@ -0,0 +1,19 @@
+
+// arrays of objects
+
+let library = [
+    {
+        title: "Wiil waal",
+        author: "Ahmed",
+        year: 2005,
+    },
+
+    {
+        title: "how to become AI engineer",
+        author: "farah warsame geedi",
+        year: 2025,
+    }
+]
+
+console.log(library[0] .title)
+console.log(library[1] .author)
