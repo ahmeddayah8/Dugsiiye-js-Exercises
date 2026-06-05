dab1e524f24981f490670aedde903582628d888f
ahmeddayah8 <ahmeddayah449@gmail.com>
2026-06-05 23:11:05 +0100

    Exercise 9

---
+++ Exercise 9/main.js
@@ -0,0 +1,12 @@
+// methods in objects exercise
+
+let car = {
+    make: "toyota",
+    year: 2025,
+    model: "V8",
+    start : function() {
+    console.log("the car has started")
+  }
+}
+
+car.start();
