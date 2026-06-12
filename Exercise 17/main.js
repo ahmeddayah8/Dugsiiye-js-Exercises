1cc1f1cf6a87e5d5fd52b391ef82e48738b8e9a6
ahmeddayah8 <ahmeddayah449@gmail.com>
2026-06-12 19:49:43 +0100

    Exercise 17

---
+++ Exercise 17/main.js
@@ -0,0 +1,12 @@
+// exercise else if
+
+let temperature = 25;
+if (temperature < 0) {
+  console.log("very cold");
+} else if (temperature < 15) {
+  console.log("cold");
+} else if (temperature < 25) {
+  console.log("warm");
+} else {
+  console.log("hot");
+}
