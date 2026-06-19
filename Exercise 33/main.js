c24cf9630f9d8bc930ec5201a8da3cae30438a09
ahmeddayah8 <ahmeddayah449@gmail.com>
2026-06-19 19:18:18 +0100

    Exercise 33

---
+++ Exercise 33/main.js
@@ -0,0 +1,12 @@
+// Exercise for manipulating
+
+const greet = document.querySelector("#greet");
+const container = document.querySelector(".container");
+
+function changeContent() {
+  greet.textContent = " Ahmed welcome to the team";
+}
+
+function changeElement() {
+  container.innerHTML = '<strong>save ma ahoo macalimow</strong>';
+}
