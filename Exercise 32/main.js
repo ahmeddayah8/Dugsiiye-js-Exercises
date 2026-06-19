a6ed930d65fd479d696bee10c1880b99587009f1
ahmeddayah8 <ahmeddayah449@gmail.com>
2026-06-19 17:54:40 +0100

    Exercise 32

---
+++ Exercise 32/main.js
@@ -0,0 +1,10 @@
+// exercise for selecting elements
+
+const greet = document.querySelector('#greet');
+console.log(greet)
+
+const des = document.querySelectorAll('.des')
+console.log(des)
+
+const button = document.querySelector('button')
+console.log(button)
